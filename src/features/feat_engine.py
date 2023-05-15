4d8fce1a6ed9aa49282b717b9746861d7fc3fc9b
Chan, Yan Chak Christopher <yanchakchristopher.chan@iss-esg.com>
2023-05-15 12:24:59 +0200

    feat(v0.1.2): argparse in function
    Argparse wrapped in function, tqdm for progress

--- src/features/feat_engine.py
+++ src/features/feat_engine.py
@@ -1,7 +1,7 @@
 ####################################################
 #Demonstration of XGBoost to predict Merc TCFD data#
 #Maintainer: Christopher Chan                      #
-#Version: 0.1.1                                    #
+#Version: 0.1.2                                    #
 #Date: 2023-05-15                                  #
 ####################################################
 
@@ -13,27 +13,28 @@ import numpy as np
 import pyarrow as pa
 import pandas as pd
 
-import matplotlib.pyplot as plt
-import seaborn as sns
+from tqdm import tqdm
 
-parser = argparse.ArgumentParser(description = "Input ESData in parquet for TCFD subset and pivot")
-parser.add_argument("--ESData", type=pathlib.Path, required=True,
-                    help="Please specify path to typesetted ESData parquet file")
-parser.add_argument("--AgentID", type=int, required=False, nargs="+",
-                    help="Please provide an optional list of AgentID to be subset")
-parser.print_help()
-args = parser.parse_args()
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description = "Input ESData in parquet for TCFD subset and pivot")
+    parser.add_argument("--ESData", type=pathlib.Path, required=True,
+                        help="Please specify path to typesetted ESData parquet file")
+    parser.add_argument("--AgentID", type=int, required=False, nargs="+",
+                        help="Please provide an optional list of AgentID to be subset")
+    parser.print_help()
+    args = parser.parse_args()
+    
+    for arg in vars(args):
+        print("\t", args, getattr(args, arg))
 
-for arg in vars(args):
-    print("\t", args, getattr(args, arg))
-
+    return(args)
+    
 # Setting paths
 data_raw = pathlib.Path("../../data/raw")
 data_interim = pathlib.Path("../../data/interim")
 data_processed = pathlib.Path("../../data/processed")
 data_external = pathlib.Path("../../data/external")
 
-ESData = pd.read_parquet(args.ESData, engine = "pyarrow", dtype_backend = "pyarrow")
 
 
 def scope_extract(df: pd.DataFrame, AgentID: list[int]|None=None) -> pd.DataFrame:
@@ -60,6 +61,8 @@ def pivot_table(unpivot_df: pd.DataFrame) -> pd.DataFrame:
     return(pivot_df)
 
 def main():
+    args = parse_arguments()
+    ESData = pd.read_parquet(args.ESData, engine = "pyarrow", dtype_backend = "pyarrow")
     Clim_extract = scope_extract(ESData, args.AgentID)
 
     # Check if TCFD_pivot in data_interim exist
@@ -81,7 +84,7 @@ def main():
         agent_list = Clim_extract["AgentId"].unique()
         print(agent_list)
 
-        for i in agent_list:
+        for i in tqdm(agent_list):
             AID_Clim = Clim_extract[Clim_extract["AgentId"] == i]
             AgentName = AID_Clim["AgentName"].values[0]
             AgentName = AgentName.replace("/", "_").replace(" ", "_")
